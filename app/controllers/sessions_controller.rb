63b8a0b98feddf6b18ab51b5499a641d6a8b5e91
Suchitra Naik <susanaik@gmail.com>
2018-08-15 08:50:35 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if params[:name] && params[:name] != ''
+      session[:name] = params[:name]
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+
   end
 
   def destroy
+    session.delete :name
+    redirect_to '/login'
   end
 
 end
