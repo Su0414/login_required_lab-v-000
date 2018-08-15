2e1e5f23dee54d7c65c1acb8dd01727cbf098054
Suchitra Naik <susanaik@gmail.com>
2018-08-15 09:13:43 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -5,7 +5,7 @@ class SecretsController < ApplicationController
     if require_login && current_user
       render :show
     else
-      redirect_to '/login'
+      redirect_to '/sessions/new'
     end
 
   end
